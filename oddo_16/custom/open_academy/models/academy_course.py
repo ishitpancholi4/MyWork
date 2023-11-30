1e5aef484d9b747d816005980de0cd47598bfb00
ishit <ishit@gmail.com>
2023-11-30 09:55:17 +0530

    first

--- oddo_16/custom/open_academy/models/academy_course.py
+++ oddo_16/custom/open_academy/models/academy_course.py
@@ -5,30 +5,31 @@ from odoo import models, fields, api, exceptions
 class AcademyCourse(models.Model):
     _name = 'academy.course'
     _description = ' OpenAcademy Course'
-    _rec_name =  'course_name'
+    _rec_name = 'course_name'
 
     course_name = fields.Char(string='Academy Course', help='Select Your Course')
-    course_description = fields.Text(string='Course Description')
+    course_description = fields.Text(string='Course Description', help='Course Descriptions')
 
     responsible_id = fields.Many2one('res.users', ondelete='set null', string='Responsible', index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string='Sessions')
 
-    def copy(self,default=None):
+    # This Fucntion use for copy and search count for field course_name
+    def copy(self, default=None):
         default = dict(default or {})
-        copied_count = self.search_count([("course_name","=like",u"Copy of {}%".format(self.course_name))])
+        copied_count = self.search_count([("course_name", "=like", u"Copy of {}%".format(self.course_name))])
         if not copied_count:
             new_name = u"Copy of {}".format(self.course_name)
         else:
-            new_name = u"Copy of {} ({})".format(self.course_name,copied_count)
+            new_name = u"Copy of {} ({})".format(self.course_name, copied_count)
         default["course_name"] = new_name
-        return super(AcademyCourse,self).copy(default)
+        return super(AcademyCourse, self).copy(default)
 
 
 class OpenAcademySession(models.Model):
     _name = 'openacademy.session'
     _rec_name = 'session_name'
 
-    session_name = fields.Char(string="Session Name", required=True)
+    session_name = fields.Char(string="Session Name", required=True, help='Enter Session Name')
     start_date = fields.Date(string="Start Date", default=fields.Date.today)
     end_date = fields.Date(string="End Date", store=True, compute="_get_end_date", inverse="_set_end_date")
     duration = fields.Float(string="Duration", digits=(6, 2), help="Duration in days")
@@ -43,6 +44,7 @@ class OpenAcademySession(models.Model):
     attendees_count = fields.Integer(string="Attendees Count", compute="_get_attendees_count", store=True)
     color = fields.Char()
 
+    # This function use for seats length
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
@@ -51,12 +53,12 @@ class OpenAcademySession(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
-
     @api.depends('attendee_ids')
     def _get_attendees_count(self):
         for r in self:
             r.attendees_count = len(r.attendee_ids)
 
+    #this function use for verify valid seats
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
@@ -94,10 +96,10 @@ class OpenAcademySession(models.Model):
             # so add one day to get 5 days instead
             rec.duration = (rec.end_date - rec.start_date).days + 1
 
-
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for rec in self:
             if rec.instructor_id in rec.attendee_ids:
                 raise exceptions.ValidationError("A session's instructor cannot be an attendee!!")
+
 
